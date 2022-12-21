37fef437a059ae937094f02fba0a68fae29a082b
X104n <github.freeware171@passmail.net>
2022-12-21 16:37:19 +0100

    Merged technical with master and made some changes so sub directories is possible in the website

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,21 @@ import {Link} from 'react-router-dom';
 
 const Index = () => {
     return (
-        <div>
-            <h1>Welcome to GeeksforGeeks</h1>
-            <Link to="/about">About</Link>
-        </div>
+        <html>
+        <head>
+            <title>Game Stats Central</title>
+        </head>
+        <body>
+        <h1>Welcome to Game Stats Central</h1>
+        <p>On this website, you will find statistics about all your favorite games. Choose from the options below to get started:</p>
+        <ul>
+            <li><Link to={'/lotto/index'}>Lotto</Link></li>
+            <li><Link to={'/viking/index'}>Viking lotto</Link></li>
+            <li><Link to={'/euro/index'}>Euro-jackpot</Link></li>
+            <li><Link to={'/about'}>About</Link></li>
+        </ul>
+        </body>
+        </html>
     );
 };
 
--- src/pages/viking/index.js
+++ src/pages/viking/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-const About = () => {
+const Viking = () => {
     return (
         <div>
             <h1>
-                This is a webpage about me
+                Viking Lotto
             </h1>
             <Link to="/">Home</Link>
         </div>
     );
 };
 
-export default About;
+export default Viking;
