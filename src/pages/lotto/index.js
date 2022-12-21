72c9ad56298ea266d61913de0fb7e51b34837306
X104n <github.freeware171@passmail.net>
2022-12-21 17:10:58 +0100

    MVP nr.1 all data from default lottery can be read by the user in the console

--- src/pages/lotto/index.js
+++ src/pages/lotto/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import SecondFile from "../../getAPIData";
 
 const Lotto = () => {
     return (
         <div>
             <h1>
-                Lotto
+                <SecondFile />
             </h1>
             <Link to="/">Home</Link>
         </div>
