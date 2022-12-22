e7e38d74cfcb7a8053376f17848ee9d6b89a408b
X104n <github.freeware171@passmail.net>
2022-12-22 21:25:36 +0100

    MVP you can now check the most choosen numbers

--- src/pages/lotto/chart.js
+++ src/pages/lotto/chart.js
@@ -17,31 +17,42 @@ ChartJS.register(BarElement,
 );
 
 export const Chart = (props) => {
+
     const data = props.data;
-    const numbers = [1, 2, 3, 4]
-/**
-    gatherNumbers();
+    const numbers = [1, 2, 3]
 
     function gatherNumbers() {
         let numbers = [];
-        for (let i = 1; i < data.length; i++) {
-            data[i].winnerNumber.forEach((number) => {
-                    numbers.push(number)
+        for (let i = 0; i < data.length; i++) {
+            data[i].winnerNumber.forEach(winnerNumbers => {
+                    numbers.push(winnerNumbers['number'])
                 }
             )
         }
-        console.log(numbers)
-        console.log(data[0].winnerNumber)
         return numbers;
     }
-*/
+
+    function countNumbers(numbers) {
+        let data = [];
+        for (let i = 1; i <= 34; i++) {
+            let count = 0;
+            for (let j = 0; j < numbers.length; j++) {
+                if (i == numbers[j]) {
+                    count++;
+                }
+            }
+            data.push(count);
+        }
+        return data;
+    }
+
     const input = {
         // Can you make the labels to through the number 1 to 34
-        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34"],
+        labels: Array.from({length: 34}, (value, index) => index + 1),
         datasets: [
             {
                 label: "Lotto",
-                data: numbers,
+                data: countNumbers(gatherNumbers()),
                 backgroundColor: 'rgba(255, 99, 132, 0.2)',
                 borderColor: 'rgba(255, 99, 132, 1)',
                 borderWidth: 1,
