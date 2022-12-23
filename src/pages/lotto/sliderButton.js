40147692002b16001ca49340fa3c6932bc22cd3c
X104n <github.freeware171@passmail.net>
2022-12-24 00:12:35 +0100

    Implemented a slider button

--- src/pages/lotto/sliderButton.js
+++ src/pages/lotto/sliderButton.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useState} from 'react';
 
 const SliderButton = () => {
+
+    const [value, setValue] = useState(2022);
+
     return (
         <div>
-            <label htmlFor="slider">Slider:</label>
-            <input type="range" id="slider" min="0" max="100" value="50"></input>
+            <label htmlFor="slider">Slider value: {value}</label>
+            <input
+                type="range"
+                id="slider"
+                min="2012"
+                max="2022"
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
+            />
         </div>
     )
 }
