a5bab671ac9714d15f95c112bc31e56a8a262db6
X104n <github.freeware171@passmail.net>
2022-12-24 01:29:23 +0100

    Structure in the css files, still some adjustments to do.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import "./styles.css"
+
+import App from './App';
+
+import "./styles/styles.css"
+import './styles/button-17.css'
+import './styles/slider.css'
+import './styles/lotto.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
